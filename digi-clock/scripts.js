9121e219ce884825ea310c5eba0f8779545a6e4c
Rodrigo Mendes <rodd.olmendes@gmail.com>
2022-09-29 23:52:51 -0400

    comments and explanations added

--- digi-clock/scripts.js
+++ digi-clock/scripts.js
@@ -1,20 +1,22 @@
+//creating function for the clock
 function Time() {
+    //creating object of Date Class
   var date = new Date();
-
+    //getting current hour
   var hour = date.getHours();
-
+    //getting current minute
   var minute = date.getMinutes();
-
+    //getting current second
   var second = date.getSeconds();
-
-  var period = " ";
-
+    //creating a variable to store AM/PM
+  var period = "";
+    //assigning AM/PM accoridng to current hour
   if (hour >= 12) {
     period = "PM";
   } else {
     period = "AM";
   }
-
+    // Converting the hour in 12-hour format
   if (hour == 0) {
     hour = 12;
   } else {
@@ -22,18 +24,24 @@ function Time() {
       hour = hour - 12;
     }
   }
-
+    // Updating hour, minute, and second
+    // if they are less than 10
   hour = update(hour);
 
   minute = update(minute);
 
   second = update(second);
 
-  document.getElementById("digital-clock").innerText =
-    hour + " : " + "minute" + " : " + "second" + " " + period;
+  // Adding time element to the div
 
+  document.getElementById("digital-clock").innerText =
+    hour + " : " + minute + " : " + second + " " + period;
+  // Setting timer to 1 second (1000ms)
   setTimeout(Time, 1000);
 }
+
+//Function to Update time elements if they are lessa than 10
+//Append 0 before time elements if they are less than 10
 
 function update(t) {
   if (t < 10) {
